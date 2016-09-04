dd108ecf26be57475fa523048ecbef1be31f46ea
avr914 <arvindvjkumar@gmail.com>
2016-09-04 04:57:00 -0500

    Added custom exceptions

--- src/main/scala/com/somlib/som/Exceptions.scala
+++ src/main/scala/com/somlib/som/Exceptions.scala
@@ -3,6 +3,7 @@ package com.somlib.som
 /**
   * Created by arvind on 9/4/16.
   */
-class Exceptions {
-
+object Exceptions {
+  class SOMException(msg: String = "SOM Exception") extends Exception("SOM Exception: " + msg) {}
+  class UndefinedPropertyException(msg: String = "Undefined Property Exception") extends SOMException(msg) {}
 }
