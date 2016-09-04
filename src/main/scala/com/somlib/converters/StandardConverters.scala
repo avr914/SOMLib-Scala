88fb7b8d4d92ba9a881fbc303965e509fb0a6c75
avr914 <arvindvjkumar@gmail.com>
2016-09-04 04:56:31 -0500

    Added implicit conversions for vectors and standard types

--- src/main/scala/com/somlib/converters/StandardConverters.scala
+++ src/main/scala/com/somlib/converters/StandardConverters.scala
@@ -1,8 +1,12 @@
 package com.somlib.converters
 
+import com.somlib.som.Utils
+
 /**
   * Created by arvind on 9/4/16.
   */
 object StandardConverters {
-
+  implicit def stringToBoolean(str: String): Boolean = str.toBoolean
+  implicit def arrayTakeSample[A](array: Array[A], sampleSize: Int, lowerLimit: Int, upperLimit: Int): Array[A] =
+    Utils.takeSample[A](array, sampleSize, lowerLimit, upperLimit)
 }
--- src/main/scala/com/somlib/converters/VectorConverters.scala
+++ src/main/scala/com/somlib/converters/VectorConverters.scala
@@ -1,8 +1,21 @@
 package com.somlib.converters
 
+import breeze.linalg.{DenseVector, SparseVector, Vector}
+import com.somlib.som.Properties
+import com.somlib.converters.StandardConverters._
+import org.apache.commons.math3.linear.RealVector
+
+
 /**
   * Created by arvind on 9/4/16.
   */
-class VectorConverters {
-
+object VectorConverters {
+  implicit def arrayToBreeze(array: Array[Double]): Vector[Double] = {
+    if(Properties.getProperty("useSparse")) SparseVector[Double](array)
+    else DenseVector[Double](array)
+  }
+  implicit def apacheRealVectorToBreeze(rv: RealVector[Double]): Vector[Double] = {
+    if(Properties.getProperty("useSparse")) SparseVector[Double](rv.toArray)
+    else DenseVector[Double](rv.toArray)
+  }
 }
