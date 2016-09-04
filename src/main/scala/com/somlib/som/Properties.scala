0ee14b4c9aa861f93db6fc610442dd363b515945
avr914 <arvindvjkumar@gmail.com>
2016-09-04 04:57:40 -0500

    Added user accessible Properties singleton for global settings

--- src/main/scala/com/somlib/som/Properties.scala
+++ src/main/scala/com/somlib/som/Properties.scala
@@ -1,8 +1,19 @@
 package com.somlib.som
 
+import java.util.Properties
+
+import scala.collection.mutable.HashMap;
+
 /**
   * Created by arvind on 9/4/16.
   */
-class Properties {
-
+object Properties {
+  val properties: Properties = new Properties
+  def validProperties = properties.keySet()
+  def isValidProperty(key: String): Boolean = validProperties.contains(key)
+  def setProperty(key: String, value: String): Unit = properties.setProperty(key, value)
+  def getProperty(key: String): String = properties.getProperty(key)
+  def apply(key: String): String = getProperty(key)
+  setProperty("useSparse","false")
+  setProperty("useData","true")
 }
