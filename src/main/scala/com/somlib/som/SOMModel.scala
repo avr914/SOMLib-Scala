783b7939156a8eac842377846e17d4731fc5a422
avr914 <arvindvjkumar@gmail.com>
2016-09-05 05:39:27 -0500

    Added cached properties for reloading of properties when reading from file

--- src/main/scala/com/somlib/som/SOMModel.scala
+++ src/main/scala/com/somlib/som/SOMModel.scala
@@ -1,12 +1,16 @@
 package com.somlib.som
 
+import java.util.Properties
+
 import breeze.linalg.Vector
-import com.somlib.som.Exceptions.UndefinedPropertyException
+import com.esotericsoftware.kryo.io.{Input, Output}
+import com.esotericsoftware.kryo.{Kryo, KryoSerializable}
+import com.somlib.som.Exceptions.{DisallowedAccessException, UndefinedPropertyException}
 
 /**
   * Created by arvind on 9/2/16.
   */
-class SOMModel(neurons: Array[Neuron], nRows: Int, nCols: Int) {
+class SOMModel(neurons: Array[Neuron], nRows: Int, nCols: Int) extends Serializable {
 
   def size() = neurons.size
 
@@ -25,7 +29,19 @@ class SOMModel(neurons: Array[Neuron], nRows: Int, nCols: Int) {
   def apply(id: Int): Neuron = neurons(id)
 
   def setProperty(key: String, value: String): Unit = {
-    if(Properties.isValidProperty(key)) Properties.setProperty(key, value)
-    else throw new UndefinedPropertyException
+    if(!key.contains("model")) throw new DisallowedAccessException("Only model properties can be modified from here")
+    Properties.setProperty(key, value)
+  }
+
+  val cachedProperties: Properties = Properties.properties
+}
+
+object SOMModel {
+  def autodecayRadius(startRadius: Double, timeConst: Double, currentIter:Int, nIter:Int): Double = {
+    startRadius * Math.exp(-currentIter/timeConst)
+  }
+
+  def autodecayLearningRate(startLR: Double, currentIter:Int, nIter: Int): Double = {
+    startLR * Math.exp(-2*currentIter.toDouble/nIter)
   }
 }
