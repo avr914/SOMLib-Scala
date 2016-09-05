68bd141983baad13d8152e5508dad008437ff5f2
avr914 <arvindvjkumar@gmail.com>
2016-09-05 05:36:28 -0500

    Added access-point based exceptions for property object

--- src/main/scala/com/somlib/som/Exceptions.scala
+++ src/main/scala/com/somlib/som/Exceptions.scala
@@ -4,6 +4,7 @@ package com.somlib.som
   * Created by arvind on 9/4/16.
   */
 object Exceptions {
-  class SOMException(msg: String = "SOM Exception") extends Exception("SOM Exception: " + msg) {}
-  class UndefinedPropertyException(msg: String = "Undefined Property Exception") extends SOMException(msg) {}
+  class SOMException(msg: String) extends Exception("SOM Exception: " + msg) {}
+  class UndefinedPropertyException(msg: String) extends SOMException("Undefined Property Exception: " + msg) {}
+  class DisallowedAccessException(msg: String) extends SOMException("Disallowed Access Exception: " + msg) {}
 }
