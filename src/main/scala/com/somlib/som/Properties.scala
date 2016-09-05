6c36b701cd482e31f05107bb69f0cc0dc0ac1fb8
avr914 <arvindvjkumar@gmail.com>
2016-09-05 05:38:17 -0500

    Updated method access permissions, added load and reset property methods

--- src/main/scala/com/somlib/som/Properties.scala
+++ src/main/scala/com/somlib/som/Properties.scala
@@ -1,19 +1,29 @@
 package com.somlib.som
 
 import java.util.Properties
-
-import scala.collection.mutable.HashMap;
+import com.somlib.som.Exceptions.UndefinedPropertyException
 
 /**
   * Created by arvind on 9/4/16.
   */
 object Properties {
-  val properties: Properties = new Properties
+  private[som] val properties: java.util.Properties = new java.util.Properties
   def validProperties = properties.keySet()
-  def isValidProperty(key: String): Boolean = validProperties.contains(key)
-  def setProperty(key: String, value: String): Unit = properties.setProperty(key, value)
+  def isValidProperty(key: String): Boolean = properties.containsKey(key)
+  private[som] def setProperty(key: String, value: String): Unit = {
+    if(isValidProperty(key)) properties.setProperty(key, value)
+    else throw new UndefinedPropertyException("%s is not a defined property".format(key))
+  }
   def getProperty(key: String): String = properties.getProperty(key)
   def apply(key: String): String = getProperty(key)
-  setProperty("useSparse","false")
-  setProperty("useData","true")
+  private[som] def resetProperties: Unit = {
+    setProperty("model.useSparseVector","false")
+    setProperty("trainer.useData","true")
+  }
+  private[som] def loadModelProperties(modelProperties: java.util.Properties): Unit = {
+    properties.setProperty("model.useSparseVector",modelProperties.getProperty("model.useSparseVector"))
+  }
+  setProperty("model.useSparseVector","false")
+  setProperty("trainer.useData","true")
+
 }
